dc933e6306124a0d881109f5094fb42296257995
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2024-09-24 12:25:49 -0300

    Inserindo loop e parada quando diggitar o valor 0

--- TRABALHO PRATICO 02/main.py
+++ TRABALHO PRATICO 02/main.py
@@ -1,14 +1,26 @@
 from caminho_critico import AnalisadorCaminhoCritico
 
 def main():
-    # Solicita ao usuário o caminho do arquivo
-    caminho_arquivo = input("Digite o caminho do arquivo CSV: ")
-    
-    # Cria uma instância do analisador
-    analisador = AnalisadorCaminhoCritico(caminho_arquivo)
-    
-    # Executa a análise
-    analisador.analisar()
+    while True:
+        # Solicita ao usuário o caminho do arquivo ou para sair
+        caminho_arquivo = input("Digite o caminho do arquivo CSV ou 0 para sair: ")
+        
+        # Se o usuário digitar '0', encerra o programa
+        if caminho_arquivo == '0':
+            print("Encerrando o programa.")
+            break
+        
+        # Caso contrário, continua com a análise
+        try:
+            # Cria uma instância do analisador
+            analisador = AnalisadorCaminhoCritico(caminho_arquivo)
+            
+            # Executa a análise
+            analisador.analisar()
+        
+        # Caso o arquivo não seja encontrado, exibe uma mensagem de erro
+        except FileNotFoundError:
+            print("Arquivo não encontrado. Tente novamente.")
 
 if __name__ == "__main__":
     main()
